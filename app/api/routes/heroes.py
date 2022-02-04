b4d6f6a5a3dc5d2e4af6bce7daa2161076740205
mbarajas <manuel.jose.barajas@gmail.com>
2022-02-03 23:09:45 -0500

    adding route to get single hero

--- app/api/routes/heroes.py
+++ app/api/routes/heroes.py
@@ -5,3 +5,7 @@ router = APIRouter()
 @router.get("/", tags=["heroes"])
 async def get_heroes():
     return [{"name": "Batman"}, {"name": "Superman"}]
+
+@router.get("/{hero}", tags=["heroes"])
+async def get_hero(hero: str):
+    return {"name": hero}
